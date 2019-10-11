07aa80f354b0045932d180f308b2d0ecb3431060
Luis Dominguez <reynaldodominguez@gmail.com>
2019-10-11 11:18:00 -0400

    Final HomeWork

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,6 +2,6 @@ USE burger_db;
 
 INSERT INTO burger(name, devour) VALUES ("Cheeseburger", true);
 INSERT INTO burger(name, devour) VALUES ("BigMac", false);
-INSERT INTO burger(name, devour) VALUES ("Bacon Burger", true);
-INSERT INTO burger(name, devour) VALUES ("Chicken Burger", true);
+INSERT INTO burger(name, devour) VALUES ("Bacon Burger", false);
+INSERT INTO burger(name, devour) VALUES ("Chicken Burger", false);
 
